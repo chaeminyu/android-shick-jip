6020469eed3ced1e8b2ab998f5014e6935034c38
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2024-11-27 14:30:37 +0900

    [feat] 화면 연결 및 홈화면 오류 해결

--- app/src/main/java/com/example/shickjip/CameraActivity.kt
+++ app/src/main/java/com/example/shickjip/CameraActivity.kt
@@ -1,4 +1,22 @@
 package com.example.shickjip
 
-class CameraActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shickjip.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater) // 뷰 바인딩 초기화
+        setContentView(binding.root)
+
+        // 뒤로가기 버튼 클릭 리스너
+        binding.backButton.setOnClickListener {
+            finish() // 현재 Activity 종료
+        }
+    }
+
 }
